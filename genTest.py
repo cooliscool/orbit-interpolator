a6959232d514e5e5f88bcdbc3af171473131f070
cooliscool <sasebot@gmail.com>
2017-03-14 16:33:37 +0530

    two methods

---
+++ genTest.py
@@ -0,0 +1,3 @@
+import numpy as np 
+
+print 5 * np.array([1,1,1]) + np.array([1,2,3])
--- testq.py
+++ testq.py
@@ -8,10 +8,18 @@
 # 5) rotate the plane to the target plane coordinate system, which is the final orbit 
 # 6) calculate the keplerian orbital elements (i,raan, argument_of_periapsis, a, e)
 
+# Method 1 : using Gibb's method and averaging to find the orbit needed.
+
+
+# physical constants
+mu = 398600.0
+
+
 import matplotlib.pylab as plt
 import numpy as np
 from numpy.linalg import eig, inv
 from random import randint
+import math
 
 def readXYZ(filename):
 	x = []
@@ -34,21 +42,11 @@ def readXYZ(filename):
 	file.close();
 	return (x,y,z)
 
-def fitEllipse(x,y):
-	x = np.array(x)[:,np.newaxis]
-	y = np.array(y)[:,np.newaxis]
 
-	D = np.hstack((x*x, x*y, y*y, x, y, np.ones_like(x))); 
-	S = np.dot(D.T,D); 	
-	C = np.zeros([6,6])
-	C[0,2] = 2; C[1,1] = -1;  C[2,0] = 2;
-	E, V =  eig(np.dot(inv(S), C))
-	n = np.argmax(np.abs(E))
-	a = V[:,n]
-	return a
 
-def getPlaneDirection():
-	return
+def difference(a,b):
+	# This function assumes the input to be a 3d vector
+	return (a[0] - b[0] , a[1] - b[1] , a[2] - b[2] )
 
 def get3Coordinates(x, y, z):
 	a = len(x)
@@ -57,41 +55,8 @@ def get3Coordinates(x, y, z):
 	k  = randint(0, a-1)
 	return ( (x[i], y[i], z[i]), (x[j], y[j], z[j]) ,(x[k], y[k], z[k]) ) 
 
-def rotate():
-	return
 
-def ellipse_center(a):
-    b,c,d,f,g,a = a[1]/2, a[2], a[3]/2, a[4]/2, a[5], a[0]
-    num = b*b-a*c
-    x0=(c*d-b*f)/num
-    y0=(a*f-b*d)/num
-    return np.array([x0,y0])
-
-
-def ellipse_angle_of_rotation( a ):
-    b,c,d,f,g,a = a[1]/2, a[2], a[3]/2, a[4]/2, a[5], a[0]
-    return 0.5*np.arctan(2*b/(a-c))
-
-
-def ellipse_axis_length( a ):
-    b,c,d,f,g,a = a[1]/2, a[2], a[3]/2, a[4]/2, a[5], a[0]
-    up = 2*(a*f*f+c*d*d+g*b*b-2*b*d*f-a*c*g)
-    down1=(b*b-a*c)*( (c-a)*np.sqrt(1+4*b*b/((a-c)*(a-c)))-(c+a))
-    down2=(b*b-a*c)*( (a-c)*np.sqrt(1+4*b*b/((a-c)*(a-c)))-(c+a))
-    res1=np.sqrt(up/down1)
-    res2=np.sqrt(up/down2)
-    return np.array([res1, res2])
-
-
-def displayEllipse(a):
-	return
-
-def difference(a,b):
-	# This function assumes the input to be a 3d vector
-	return (a[0] - b[0] , a[1] - b[1] , a[2] - b[2] )
-
-
-def getPlaneDirection(a,b,c):
+def getOrbitPlaneDirection(a,b,c):
 
 	# there are two possible directions for any plane.
 	# say, if one is x, other one should be -x .
@@ -100,29 +65,83 @@ def getPlaneDirection(a,b,c):
 	# (x is a unit vector)
 
 	x = np.cross (np.array( difference(a, b )), np.array( difference(c,b) )) 
-	mod = np.dot(x,x) ** 0.5
-		
+	mod = np.linalg.norm(x)
+
 	# returns the unit vector with positive z component
 	if  x[2]/mod<0:
 		return (-1*x[0]/mod, -1*x[1]/mod, -1*x[2]/mod)
 	else : 
 		return (x[0]/mod, x[1]/mod, x[2]/mod)
 
-	
+def getInclination(h):
+	# h is the direction of orbital plane with respect to ECI frame
+	return (math.acos(h[2] /np.linalg.norm(h)) * 180.0 / math.pi)
+
+def getRaan(h):
+	# h is the direction of orbital plane with respect to ECI frame
+	n = np.cross(np.array([0,0,1]), np.array(h))
+	return (math.acos(n[0] /np.linalg.norm(n)) * 180.0 / math.pi)
+
+def getVelocity(a, b, c):
+	# Gibb's method to get velocity of three points, which we feed in order
+	# Algorithm: 
+	# 1) Get position vectors a, b, c
+	# 2) Calculate aXb, bXc, cXa
+	# 3) Verify that a.(bXc) =0
+	# 4) Calculate N, D and S
+	# 5) Calculate va, vb, vc
+	a = np.array(a)
+	b = np.array(b)
+	c = np.array(c)
+	moda = np.linalg.norm(a)
+	modb = np.linalg.norm(b)
+	modc = np.linalg.norm(c)
+	aXb = np.cross(a,b)
+	bXc = np.cross(b,c)
+	cXa = np.cross(c,a)
+
+	if abs(np.dot( a, aXb)) > 0.00001:
+		return 'vectors a, b, c are not coplanar'
+   	
+	N = aXb*modc + bXc*moda + cXa*modb
+	D = aXb + bXc + cXa
+	S = a*(modb - modc) + b*(modc - moda) + c*(moda - modb)
+
+	va = ((mu/(np.linalg.norm(N)* np.linalg.norm(D)))**0.5) * (S + np.cross (D,a)/moda) 
+	vb = ((mu/(np.linalg.norm(N)* np.linalg.norm(D)))**0.5) * (S + np.cross (D,b)/modb) 
+	vc = ((mu/(np.linalg.norm(N)* np.linalg.norm(D)))**0.5) * (S + np.cross (D,c)/modc) 
+
+	return (va, vb, vc)
+
+def getE(r,v):
+	r = np.array(r)
+	v = np.array(v)
+	modr = np.linalg.norm(r)
+	modv = np.linalg.norm(v)
+	e = ((modv**2 - (mu/modr))*r  - (np.dot(r,v))*v )/mu
+	return e
+
+def getPeriapsisArgument(h,e):
+	n = np.cross(np.array([0,0,1]), np.array(h))
+	return math.acos(  np.dot(n,e) / np.linalg.norm(n)* np.linalg.norm(e)) * 180 / math.pi
+
 if __name__ == '__main__':
 
 	x, y, z =  readXYZ("trackdata.csv");
-	a =  fitEllipse(x,y)
-
-
+	
 	plt.plot(0, 0, "o")
 	plt.plot(x, y)
 	plt.grid()
 	#plt.show()
 
 
-	a, b, c = get3Coordinates(x,y,z)
+	a, b, c = get3Coordinates(x,y,z) # returns lists a,b,c
 	
-	planeDir = getPlaneDirection(a,b,c)
+	planeDir = getOrbitPlaneDirection(a,b,c)
 
-	print planeDir
+	va, vb, vc = getVelocity(a,b,c) # returns array
+
+	e = getE(a, va) # returns array
+
+	print getPeriapsisArgument(planeDir,e)
+	
